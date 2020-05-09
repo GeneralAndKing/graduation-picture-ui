94b68c0581b117416225ebbc404f941ecd7f79fe
soul <437665122@qq.com>
2020-05-09 14:08:36 +0800

    keyboard

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,35 +1,45 @@
 //index.js
 //获取应用实例
 const app = getApp()
-import { $wuxKeyBoard } from '../../dist/index'
 Page({
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
     value: '',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
-  // 班长入口
-  master: function() {
+
+  /**
+   * 班长入口
+   */
+  master: function () {
     wx.navigateTo({
       url: '../albumCode/albumCode'
     })
   },
-  // 学生入口
-  student: function() {
+
+  /**
+   * 学生入口
+   */
+  student: function () {
     wx.navigateTo({
       url: '../studentPage/studentPage'
     })
   },
-  //以下是自带的请求头像和用户名的函数
+
+  /**
+   * 页面加载
+   */
   onLoad: function () {
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -51,27 +61,16 @@ Page({
       })
     }
   },
-  getUserInfo: function(e) {
+
+  /**
+   * 获取微信用户信息
+   */
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
-    })
-  },
-  open() {
-    $wuxKeyBoard().show({
-      className: 'className',
-      titleText: '安全键盘',
-      cancelText: '取消',
-      inputText: '输入相册码',
-      showCancel: true,
-      disorder: false,
-      maxlength: 4,
-      callback(value) {
-          console.log(`输入的密码是：${value}`)
-          return true
-      },
     })
   }
 })
